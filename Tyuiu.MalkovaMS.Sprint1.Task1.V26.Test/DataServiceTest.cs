3f2e05647ce9ac1ff900946864d1daea818a18e1
Мария Малькова <Tyuiu.MalkovaMS@outlook.com>
2025-09-23 20:55:37 +0500

    Task1

---
+++ Tyuiu.MalkovaMS.Sprint1.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MalkovaMS.Sprint1.Task1.V26.Lib;
+
+namespace Tyuiu.MalkovaMS.Sprint1.Task1.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1.25, res);
+
+        }
+    }
+}
