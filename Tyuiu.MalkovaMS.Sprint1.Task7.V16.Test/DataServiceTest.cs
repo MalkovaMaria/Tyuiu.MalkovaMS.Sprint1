ff1348fb697f84b468cb166a7860bd7214a0e3ec
Мария Малькова <Tyuiu.MalkovaMS@outlook.com>
2025-09-28 01:11:22 +0500

    Task7

---
+++ Tyuiu.MalkovaMS.Sprint1.Task7.V16.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MalkovaMS.Sprint1.Task7.V16.Lib;
+
+namespace Tyuiu.MalkovaMS.Sprint1.Task7.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            Assert.AreEqual(1.022, ds.Calculate(x));
+
+        }
+    }
+}
---
+++ Tyuiu.MalkovaMS.Sprint1.Task7.V16/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.MalkovaMS.Sprint1.Task7.V16.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Малькова М. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #16                                                             *");
+        Console.WriteLine("* Выполнила: Малькова Мария Сергеевна | ИИПб-25-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+        Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("                     cos(x^2)                                              ");
+        Console.WriteLine("z = sin(sqrt(x^2)) + --------- - sin(sqrt(x^2 - 1))                        ");
+        Console.WriteLine("                       3*x^2                                               ");
+        Console.WriteLine("***************************************************************************");
+
+        double x;
+        Console.WriteLine("Введите значение Х:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(x));
+        Console.ReadKey();
+
+    }
+}
