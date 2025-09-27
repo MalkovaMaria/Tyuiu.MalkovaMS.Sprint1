21897c19d80372767034d79029167055ca03bec2
Мария Малькова <Tyuiu.MalkovaMS@outlook.com>
2025-09-27 16:10:34 +0500

    Task4

---
+++ Tyuiu.MalkovaMS.Sprint1.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MalkovaMS.Sprint1.Task4.V14.Lib;
+
+namespace Tyuiu.MalkovaMS.Sprint1.Task4.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double wait = 0.236;
+            Assert.AreEqual(wait, ds.Calculate(x,y));
+        }
+    }
+}
---
+++ Tyuiu.MalkovaMS.Sprint1.Task4.V14/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.MalkovaMS.Sprint1.Task4.V14.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Малькова М. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Class Math                                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнила: Малькова Мария Сергеевна | ИИПб-25-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле и печатает результат на экране.          *");
+        Console.WriteLine("* ФОРМУЛА:                                                                *");
+        Console.WriteLine("* sqrt (7 + abs(x-y) / (3*x*y^2)                                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x,y;
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("sqrt (7 + abs(x-y) / (3*x*y^2) = " + ds.Calculate(x, y));
+        Console.ReadKey();
+
+    }
+}
