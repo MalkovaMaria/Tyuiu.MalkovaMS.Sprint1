ba7575d55d5e35b0a1beaeb7b528a595f955113a
Мария Малькова <Tyuiu.MalkovaMS@outlook.com>
2025-09-27 17:09:15 +0500

    Task5

---
+++ Tyuiu.MalkovaMS.Sprint1.Task5.V5.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MalkovaMS.Sprint1.Task5.V5.Lib
+{
+    public class DataService : ISprint1Task5V5
+    {
+        public int Calculate(double x)
+        {
+            double a = Math.Truncate(x * 10) / 10;
+            int d = Convert.ToInt32((a - Math.Truncate(x)) * 10);
+            return d;
+        }
+    }
+}
